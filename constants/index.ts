095070537b5beb5faf19b8388df88af8ef79ba3c
Nickson2511 <okwembanickson8@gmail.com>
2023-09-01 13:10:27 +0300

    added a search filter component

---
+++ constants/index.ts
@@ -0,0 +1,102 @@
+export const manufacturers = [
+    "Acura",
+    "Alfa Romeo",
+    "Aston Martin",
+    "Audi",
+    "Bentley",
+    "BMW",
+    "Buick",
+    "Cadillac",
+    "Chevrolet",
+    "Chrysler",
+    "Citroen",
+    "Dodge",
+    "Ferrari",
+    "Fiat",
+    "Ford",
+    "GMC",
+    "Honda",
+    "Hyundai",
+    "Infiniti",
+    "Jaguar",
+    "Jeep",
+    "Kia",
+    "Lamborghini",
+    "Land Rover",
+    "Lexus",
+    "Lincoln",
+    "Maserati",
+    "Mazda",
+    "McLaren",
+    "Mercedes-Benz",
+    "MINI",
+    "Mitsubishi",
+    "Nissan",
+    "Porsche",
+    "Ram",
+    "Rolls-Royce",
+    "Subaru",
+    "Tesla",
+    "Toyota",
+    "Volkswagen",
+    "Volvo",
+  ];
+  
+  export const yearsOfProduction = [
+    { title: "Year", value: "" },
+    { title: "2015", value: "2015" },
+    { title: "2016", value: "2016" },
+    { title: "2017", value: "2017" },
+    { title: "2018", value: "2018" },
+    { title: "2019", value: "2019" },
+    { title: "2020", value: "2020" },
+    { title: "2021", value: "2021" },
+    { title: "2022", value: "2022" },
+    { title: "2023", value: "2023" },
+  ];
+  
+  export const fuels = [
+    {
+      title: "Fuel",
+      value: "",
+    },
+    {
+      title: "Gas",
+      value: "Gas",
+    },
+    {
+      title: "Electricity",
+      value: "Electricity",
+    },
+  ];
+  
+  export const footerLinks = [
+    {
+      title: "About",
+      links: [
+        { title: "How it works", url: "/" },
+        { title: "Featured", url: "/" },
+        { title: "Partnership", url: "/" },
+        { title: "Bussiness Relation", url: "/" },
+      ],
+    },
+    {
+      title: "Company",
+      links: [
+        { title: "Events", url: "/" },
+        { title: "Blog", url: "/" },
+        { title: "Podcast", url: "/" },
+        { title: "Invite a friend", url: "/" },
+      ],
+    },
+    {
+      title: "Socials",
+      links: [
+        { title: "Medium", url: "https://medium.com/@okwembanickson" },
+        { title: "Instagram", url: "https://www.instagram.com/okwenick/" },
+        { title: "Twitter", url: "https://twitter.com/okwenick2511" },
+        { title: "Facebook", url: "https://www.facebook.com/ooh.ole.9" },
+      ],
+    },
+  ];
+  
