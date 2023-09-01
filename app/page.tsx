4f56e5de3a0a2e1df46925149813aaba4398746b
Nickson2511 <okwembanickson8@gmail.com>
2023-09-01 13:16:49 +0300

    added search bar component which allow user to search

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
-import { fetchCars } from "../utils"
+import { fetchCars } from "../utils";
 import { HomeProps } from "../types";
 import { fuels, yearsOfProduction } from "../constants";
-import { CarCard,  ShowMore, SearchBar, CustomFilter, Hero } from "../components/";
-
+import { CarCard, ShowMore, SearchBar, CustomFilter, Hero } from "../components";
 
 export default async function Home({ searchParams }: HomeProps) {
   const allCars = await fetchCars({
