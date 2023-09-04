a9d0c82746f9b78a6ececd8a61b0741a3cc048ba
Nickson2511 <okwembanickson8@gmail.com>
2023-09-04 11:17:35 +0300

    ui for vehicle system

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,16 @@ import { HomeProps } from "../types";
 import { fuels, yearsOfProduction } from "../constants";
 import { CarCard, ShowMore, SearchBar, CustomFilter, Hero } from "../components";
 
-export default async function Home({ searchParams }: HomeProps) {
+export default async function Home({searchParams}:HomeProps ) {
   const allCars = await fetchCars({
     manufacturer: searchParams.manufacturer || "",
-    year: searchParams.year || 2022,
+    year: searchParams.year || 2023,
     fuel: searchParams.fuel || "",
     limit: searchParams.limit || 10,
     model: searchParams.model || "",
   });
-
+  //lest derive a box where will structure if our cars in allCars value will be true or false
+  
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
   return (
--- utils/index.ts
+++ utils/index.ts
@@ -1,18 +1,19 @@
-import { CarProps, FilterProps } from "../types"
+import { CarProps, FilterProps } from "../types";
+export const calculateCarRent = (city_mpg: number, year: number)=>{
+  const basePricePerDay =50; //base rental per day in dollars
+  const mileageFactor = 0.1; //Additional rate per mile driven
+  const ageFactor = 0.05; //Additional rate per year of vehicle age
 
-export const calculateCarRent = (city_mpg: number, year: number) => {
-  const basePricePerDay = 50; // Base rental price per day in dollars
-  const mileageFactor = 0.1; // Additional rate per mile driven
-  const ageFactor = 0.05; // Additional rate per year of vehicle age
+  //Calculate additional rate based on milleage and age
 
-  // Calculate additional rate based on mileage and age
-  const mileageRate = city_mpg * mileageFactor;
-  const ageRate = (new Date().getFullYear() - year) * ageFactor;
-
-  // Calculate total rental rate per day
+  const mileageRate =city_mpg * mileageFactor;
+  const ageRate =(new Date().getFullYear() - year) * ageFactor;
+ 
+  //Calculate total rental rate per day
   const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
 
   return rentalRatePerDay.toFixed(0);
+
 };
 
 export const updateSearchParams = (type: string, value: string) => {
@@ -41,34 +42,40 @@ export const deleteSearchParams = (type: string) => {
   return newPathname;
 };
 
-export async function fetchCars(filters: FilterProps) {
+
+
+export async function fetchCars(filters: FilterProps){
   const { manufacturer, year, model, limit, fuel } = filters;
 
-  // Set the required headers for the API request
-  const headers: HeadersInit = {
-    "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
-    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
-  };
+  //Set the required headers for the API request
+  const headers ={
+    
+    'X-RapidAPI-Key': '1cd03de230msh60c3083aa6cd9e5p12545djsn7ad308e32a66',
+		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+  }
+  //lets set the required headers for the API request
+  const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera',
+  {
+    headers:headers,
+   }
 
-  // Set the required headers for the API request
-  const response = await fetch(
-    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
-    {
-      headers: headers,
-    }
   );
+  //Parse the response as JSON
 
-  // Parse the response as JSON
-  const result = await response.json();
+  const result =await response.json();
 
   return result;
+
+  
 }
+
+//confirm the below image url with the respective api fetch
 
 export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   const url = new URL("https://cdn.imagin.studio/getimage");
   const { make, model, year } = car;
 
-  url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '');
+  url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '7mC26u/bbIepIfu7YLtiSg==OTbYt7yBRx923zDc');
   url.searchParams.append('make', make);
   url.searchParams.append('modelFamily', model.split(" ")[0]);
   url.searchParams.append('zoomType', 'fullscreen');
@@ -77,4 +84,7 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   url.searchParams.append('angle', `${angle}`);
 
   return `${url}`;
-} 
+}
+
+
+
